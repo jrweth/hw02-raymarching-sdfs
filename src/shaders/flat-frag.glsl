691010c19303fee4b711811bda38319a61d99a58
J. Reuben Wetherbee <wetherbe@sas.upenn.edu>
2019-02-03 15:57:04 -0500

    got first render working

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -9,5 +9,19 @@ in vec2 fs_Pos;
 out vec4 out_Col;
 
 void main() {
-  out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
+
+
+  float aspect = u_Dimensions.x / u_Dimensions.y;
+
+  vec3 U = u_Up;
+  vec3 R = normalize(cross( u_Ref - u_Eye, u_Up));
+  float len = length(u_Ref - u_Eye);
+  vec3 V = u_Up * len; //normally this would also be based upon FOV tan(FOV) but we are constraing to the box
+  vec3 H = R * aspect * len; //normally this would also be based upon FOV tan(FOV) but we are constraining to the box
+
+  vec3 p = u_Ref + (fs_Pos.x * H) + fs_Pos.y * V;
+
+  vec3 ray = normalize(p - u_Eye);
+
+  out_Col = vec4(0.5 * (ray + vec3(1.0, 1.0, 1.0)), 1.0);
 }

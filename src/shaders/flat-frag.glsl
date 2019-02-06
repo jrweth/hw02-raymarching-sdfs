e01fee7c189929de7b44881b9bda1c3b91f2222d
J. Reuben Wetherbee <wetherbe@sas.upenn.edu>
2019-02-05 22:29:41 -0500

    added first pass at bvh

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -9,7 +9,11 @@ in vec2 fs_Pos;
 out vec4 out_Col;
 
 const vec3 lightDirection = normalize(vec3(1.0,2.0,-1.0));
+const float sceneRadius = 50.0;
+const int numObjects = 5;
+const float distanceThreshold = 0.001;
 
+// Params definining an sdf in our scene
 struct sdfParams {
     int sdfType;
     int textureType;
@@ -17,7 +21,135 @@ struct sdfParams {
     float radius;
     int numCraters;
     vec3 color;
+    vec3 size;
 };
+
+sdfParams sdfs[numObjects];
+
+//
+struct boundingCube {
+   int parentIndex;
+   int numObjects;
+   vec3 min;
+   vec3 max;
+};
+
+
+boundingCube bCubes1;
+boundingCube[8] bCubes2;
+boundingCube[64] bCubes3;
+boundingCube[numObjects] bCubes4;
+
+
+//This function breaks the scene up into bounding cubes
+//the cubes are on three levels:
+// level 1 - the whole scene
+// level 2 - 8 octants of whole scene
+// level 3 - 8 octants of each of the level 2 octants
+void initializeBoundingCubes() {
+    //initialize the big cube
+    bCubes1.numObjects = 0;
+    bCubes1.min = vec3(0.0, 0.0, 0.0);
+    bCubes1.max = vec3(0.0, 0.0, 0.0);
+
+    //initialize the second level cubes
+    for(int i = 0; i < 8; i++) {
+       bCubes2[i].numObjects = 0;
+       float i2 = float(i);
+       //set the minimum and max to the "lowest" corner of each cube
+       float xmin1 = -sceneRadius + mod(i2, 2.0) * sceneRadius;
+       float ymin1 = -sceneRadius + floor(mod(i2/2.0, 2.0)) * sceneRadius;
+       float zmin1 = -sceneRadius + floor(i2/4.0) * sceneRadius;
+       bCubes2[i].min = vec3(xmin1, ymin1, zmin1);
+       bCubes2[i].max = vec3(xmin1, ymin1, zmin1);
+
+       //initialize the third level cubes
+       for(int j = 0; j < 8; j++) {
+           int index = i*8 + j;
+           float j2 = float(j);
+           float xmin2 = xmin1 + mod(j2, 2.0) * sceneRadius / 2.0;
+           float ymin2 = ymin1 + floor(mod(j2/2.0, 2.0)) * sceneRadius / 2.0;
+           float zmin2 = zmin1 + floor(j2/4.0) * sceneRadius;
+           bCubes3[index].numObjects = 0;
+           bCubes3[index].min = vec3(xmin2, ymin2, zmin2);
+           bCubes3[index].max = vec3(xmin2, ymin2, zmin2);
+           bCubes3[index].parentIndex = i;
+       }
+    }
+}
+
+int getBoundingCube2Index(vec3 point) {
+    //check to make sure it is in our scene
+    if(abs(point.x) > sceneRadius) return - 1;
+    if(abs(point.y) > sceneRadius) return - 1;
+    if(abs(point.z) > sceneRadius) return - 1;
+
+    int index = 0;
+    if(point.x >= 0.0) index++;
+    if(point.y >= 0.0) index += 2;
+    if(point.z >= 0.0) index += 4;
+
+    return index;
+}
+
+//get the third level bounding index where point resides
+int getBoundingCube3Index(vec3 point) {
+    int index = getBoundingCube2Index(point);
+    if(index == -1) return -1;
+
+    index = index * 8;
+    index += 1 * int(floor(2.0 * mod(point.x, sceneRadius) / sceneRadius));
+    index += 2 * int(floor(2.0 * mod(point.y, sceneRadius) / sceneRadius));
+    index += 4 * int(floor(2.0 * mod(point.z, sceneRadius) / sceneRadius));
+
+    return index;
+}
+
+boundingCube getBoundingCubeForObject(sdfParams params) {
+    boundingCube bCube;
+    switch(params.sdfType) {
+        case 1: //sphere
+        case 2: //moon
+        case 3: //shere intersect
+        case 4: //sphere smooth blend
+            bCube.min = params.center - vec3(params.radius);
+            bCube.max = params.center + vec3(params.radius);
+    }
+    return bCube;
+}
+
+void addChildToBoundingCube(in boundingCube child, inout boundingCube parent) {
+    parent.min.x = min(parent.min.x, child.min.x);
+    parent.min.y = min(parent.min.y, child.min.y);
+    parent.min.z = min(parent.min.z, child.min.z);
+
+    parent.max.x = max(parent.max.x, child.max.x);
+    parent.max.y = max(parent.max.y, child.max.y);
+    parent.max.z = max(parent.max.z, child.max.z);
+
+   parent.numObjects ++;
+
+}
+
+
+void addObjectToBoundingCubes(int objectIndex) {
+    sdfParams params = sdfs[objectIndex];
+    boundingCube bc = getBoundingCubeForObject(params);
+
+    addChildToBoundingCube(bc, bCubes1);
+
+    int b2Index = getBoundingCube2Index(params.center);
+    addChildToBoundingCube(bc, bCubes2[b2Index]);
+
+    //set the b3 cube
+    int b3Index = getBoundingCube3Index(params.center);
+    addChildToBoundingCube(bc, bCubes3[b3Index]);
+
+    //set the b4 cube
+    bc.parentIndex = b3Index;
+    bCubes4[objectIndex].parentIndex = b3Index;
+}
+
 
 
 //float random1( vec2 p , vec2 seed) {
@@ -88,6 +220,13 @@ float sphereSDF(sdfParams params, vec3 point) {
 
 vec3 sphereNormal(sdfParams params, vec3 point) {
     return normalize(point - params.center);
+}
+
+sdfParams boundingCubeToSdfParams(boundingCube bc) {
+    sdfParams params;
+    params.center = mix(bc.min, bc.max, 0.5);
+    params.size = bc.max - bc.min;
+    return params;
 }
 
 
@@ -181,6 +320,14 @@ float sphereSmoothBlendSDF(sdfParams params, vec3 point) {
 
 }
 
+float cubeSDF(sdfParams params, vec3 point) {
+     //translate points so that cube is at origin
+     point -= params.center;
+     vec3 d = abs(point) - params.size;
+     return length(max(d, 0.0))
+        + min(max(d.x,max(d.y,d.z)),0.0);
+}
+
 float rayMarch(sdfParams params, vec3 ray, int maxIterations, float maxT) {
     float t = 0.0;
     vec3 rayPos;
@@ -196,10 +343,11 @@ float rayMarch(sdfParams params, vec3 ray, int maxIterations, float maxT) {
             case 1: distance = moonSDF             (params, rayPos); break;
             case 2: distance = sphereIntersectSDF  (params, rayPos); break;
             case 3: distance = sphereSmoothBlendSDF(params, rayPos); break;
+            case 4: distance = cubeSDF             (params, rayPos); break;
         }
 
         //if distance < some epsilon we are done
-        if(distance < 0.001) {
+        if(distance < distanceThreshold) {
             return t;
         }
 
@@ -233,11 +381,25 @@ vec3 getNormalFromRays(sdfParams params) {
 }
 
 
-vec3 sphereSmoothBlendNormal(sdfParams params, vec3 point) {
-    return getNormalFromRays(params);
+
+vec3 cubeNormal(sdfParams params, vec3 point) {
+    //return getNormalFromRays(params);
+    if(abs(params.center.x + params.size.x - point.x) <= distanceThreshold*10.0) return vec3(1.0, 0.0, 0.0);
+    if(abs(params.center.x - params.size.x - point.x) <= distanceThreshold*10.0) return vec3(-1.0, 0.0, 0.0);
+
+    if(abs(params.center.y + params.size.y - point.y) <= distanceThreshold) return vec3(0.0, 1.0, 0.0);
+    if(abs(params.center.y - params.size.y - point.y) <= distanceThreshold) return vec3(0.0, -1.0, 0.0);
+
+    if(abs(params.center.z + params.size.z - point.z) <= distanceThreshold) return vec3(0.0, 0.0, 1.0);
+    if(abs(params.center.z - params.size.z - point.z) <= distanceThreshold) return vec3(0.0, 0.0, -1.0);
+
+    return vec3(1.0);
 }
 
 
+vec3 sphereSmoothBlendNormal(sdfParams params, vec3 point) {
+    return getNormalFromRays(params);
+}
 
 
 vec3 getNormal(sdfParams params, vec3 point) {
@@ -246,6 +408,7 @@ vec3 getNormal(sdfParams params, vec3 point) {
         case 1: return moonNormal              (params, point);
         case 2: return sphereIntersectNormal   (params, point);
         case 3: return sphereSmoothBlendNormal (params, point);
+        case 4: return cubeNormal              (params, point);
     }
     return vec3(0.0, 0.1, 0.0);
 }
@@ -259,26 +422,10 @@ vec4 getTextureColor(sdfParams params, vec3 point) {
             float intensity = dot(normal, lightDirection) * 0.9 + 0.1;
             return vec4(params.color * intensity, 1.0);
     }
+    return vec4(params.color, 1.0);
 }
 
-
-
-
-
-void main() {
-
-
-    float aspect = u_Dimensions.x / u_Dimensions.y;
-
-    //calculate the ray
-    vec3 ray = getRay(u_Up, u_Eye, u_Ref, aspect, fs_Pos);
-
-    //set the default background color
-    vec4 color = vec4(0.5 * (ray + vec3(1.0, 1.0, 1.0)), 1.0);
-
-    //define all of our shapes
-    sdfParams sdfs[4];
-
+void initSdfs() {
     //sphere
     sdfs[0].center = vec3(0.0, 0.0, 0.0);
     sdfs[0].radius = 2.0;
@@ -309,14 +456,50 @@ void main() {
     sdfs[3].textureType = 0;
     sdfs[3].color = vec3(0.0, 0.0, 1.0);
 
+    //square
+    sdfs[4].center = vec3(8.5, 0.0, 0.0);
+    sdfs[4].radius = 2.0;
+    sdfs[4].sdfType = 4;
+    sdfs[4].textureType = 0;
+    sdfs[4].color = vec3(0.3, 0.6, 1.0);
+    sdfs[4].size = vec3(2.0);
+}
+
+
+
+
+
+void main() {
+
+
+    float aspect = u_Dimensions.x / u_Dimensions.y;
+
+    //calculate the ray
+    vec3 ray = getRay(u_Up, u_Eye, u_Ref, aspect, fs_Pos);
+
+    //set the default background color
+    vec4 color = vec4(0.5 * (ray + vec3(1.0, 1.0, 1.0)), 1.0);
+
+
     //set up
+    initSdfs();
+    initializeBoundingCubes();
+
     float maxT = 100.0;
     int maxIterations = 100;
     vec3 normal = lightDirection;
     float t;
 
     //ray march each sdf to determine the
-    for(int i = 0; i < 4; i++) {
+    sdfParams params = boundingCubeToSdfParams(bCubes1);
+    t = rayMarch(params, ray, maxIterations, maxT);
+    if(ray.x < bCubes1.max.z) {
+        //get the diffuse term
+        color = getTextureColor(params, u_Eye + ray*t);
+        maxT = t;
+    }
+
+    for(int i = 0; i < numObjects; i++) {
         t = rayMarch(sdfs[i], ray, maxIterations, maxT);
 
         if( t < maxT) {
